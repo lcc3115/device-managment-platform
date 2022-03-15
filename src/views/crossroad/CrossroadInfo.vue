65cc2e000fdf4743cd57eb11d3ba23393ae230c7
xby020 <xby020@163.com>
2022-03-15 08:50:14 +0800

    feat(view/crossroad): Add road info.

--- src/views/crossroad/CrossroadInfo.vue
+++ src/views/crossroad/CrossroadInfo.vue
@@ -55,27 +55,84 @@
         </n-tab-pane>
 
         <!-- 渠化和设施 -->
-        <n-tab-pane name="road" tab="渠化和设施">
+        <n-tab-pane name="road" tab="渠化">
           <n-form ref="roadForm" :model="res">
-            <n-grid cols="2">
-              <!-- 设施 -->
-              <n-gi class="p-1">
-                <!-- 信号机品牌 -->
-                <n-form-item-gi label="信号机品牌" path="intersection">
-                  <n-select
-                    v-model:value="res.signal_brand"
-                    placeholder="信号机品牌"
-                    :options="signalBrandOpts"
-                  />
-                </n-form-item-gi>
-              </n-gi>
-
+            <n-grid cols="4">
               <!-- 渠化 -->
-              <n-gi class="p-1">
+              <n-gi :span="3" class="p-1">
                 <n-card :bordered="false" content-style="padding: 0;">
                   <RoadList :road-list="res.roadList" />
                 </n-card>
               </n-gi>
+            </n-grid>
+          </n-form>
+        </n-tab-pane>
+
+        <!-- 设施 -->
+        <n-tab-pane name="device" tab="设施">
+          <n-form class="w-2/3">
+            <n-grid :cols="4" :x-gap="32">
+              <!-- 信号机品牌 -->
+              <n-form-item-gi :span="4" label="信号机品牌" path="intersection">
+                <n-select
+                  v-model:value="res.signal_brand"
+                  placeholder="信号机品牌"
+                  :options="signalBrandOpts"
+                />
+              </n-form-item-gi>
+
+              <n-form-item-gi :span="2" v-for="(device, deviceIndex) in subList" :key="deviceIndex">
+                <n-card :title="getDeviceName(device.attribute)">
+                  <n-grid :cols="4" :x-gap="8">
+                    <n-form-item-gi :span="2" label="经度">
+                      <n-input
+                        v-model:value="device.longitude"
+                        type="text"
+                        placeholder="经度"
+                        size="medium"
+                        clearable
+                      />
+                    </n-form-item-gi>
+                    <n-form-item-gi :span="2" label="纬度">
+                      <n-input
+                        v-model:value="device.latitude"
+                        type="text"
+                        placeholder="纬度"
+                        size="medium"
+                        clearable
+                      />
+                    </n-form-item-gi>
+                    <n-form-item-gi label="东">
+                      <n-switch
+                        v-model:value="device.east"
+                        checked-value="有"
+                        unchecked-value="无"
+                      />
+                    </n-form-item-gi>
+                    <n-form-item-gi label="南">
+                      <n-switch
+                        v-model:value="device.south"
+                        checked-value="有"
+                        unchecked-value="无"
+                      />
+                    </n-form-item-gi>
+                    <n-form-item-gi label="西">
+                      <n-switch
+                        v-model:value="device.west"
+                        checked-value="有"
+                        unchecked-value="无"
+                      />
+                    </n-form-item-gi>
+                    <n-form-item-gi label="北">
+                      <n-switch
+                        v-model:value="device.north"
+                        checked-value="有"
+                        unchecked-value="无"
+                      />
+                    </n-form-item-gi>
+                  </n-grid>
+                </n-card>
+              </n-form-item-gi>
             </n-grid>
           </n-form>
         </n-tab-pane>
@@ -87,7 +144,7 @@
 <script lang="ts">
   import { defineComponent, ref } from 'vue';
   import { FormInst } from 'naive-ui';
-  import RoadList from './components/roadList.vue';
+  import RoadList from './components/RoadList.vue';
   export default defineComponent({
     components: {
       RoadList,
@@ -96,142 +153,143 @@
       // get crossinfo
       const res = ref({
         crossing_id: 42010001,
-        intersection: '十字型',
-        Channel: '有',
-        width_Sections_road: '4',
+        intersection: 'T型',
+        Channel: '无',
+        width_Sections_road: '3',
         width_interse_road: '3',
-        crossing_name: '测试路口',
-        longitude: '30.1234',
-        latitude: '114.1234',
-        organiz: '借道左转',
-        signal_brand: '海信',
-        road_paving: '沥青',
-        submit_time: '2022-02-1 15:00:00',
+        crossing_name: '插入测试路口',
+        longitude: '114.89745',
+        latitude: '30.56789',
+        organiz: '可变车道',
+        signal_brand: '西门子',
+        road_paving: '水泥',
+        submit_time: '2022-03-09 10:33:41',
         operater: '0',
         roadList: [
           {
             direction: '东',
-            export_numb: '3',
-            import_numb: '4',
-            straight: '0',
+            export_numb: '4',
+            import_numb: '5',
+            straight: '2',
             left: '1',
             right: '1',
             turnaround: '0',
-            left_turn: '1',
-            straight_left: '1',
-            straight_right: '0',
+            left_turn: '0',
+            straight_left: '0',
+            straight_right: '1',
+            left_straight_right: '0',
+            variation: '0',
+          },
+          {
+            direction: '南',
+            export_numb: '4',
+            import_numb: '5',
+            straight: '2',
+            left: '1',
+            right: '1',
+            turnaround: '0',
+            left_turn: '0',
+            straight_left: '0',
+            straight_right: '1',
             left_straight_right: '0',
             variation: '0',
           },
           {
             direction: '西',
-            export_numb: '2',
-            import_numb: '4',
-            straight: '0',
-            left: '1',
-            right: '0',
-            turnaround: '1',
-            left_turn: '1',
-            straight_left: '0',
-            straight_right: '0',
-            left_straight_right: '1',
-            variation: '0',
-          },
-          {
-            direction: '南',
-            export_numb: '3',
-            import_numb: '4',
-            straight: '0',
+            export_numb: '4',
+            import_numb: '5',
+            straight: '2',
             left: '1',
             right: '1',
             turnaround: '0',
             left_turn: '0',
-            straight_left: '1',
-            straight_right: '0',
+            straight_left: '0',
+            straight_right: '1',
             left_straight_right: '0',
-            variation: '1',
+            variation: '0',
           },
           {
             direction: '北',
             export_numb: '4',
-            import_numb: '4',
-            straight: '0',
+            import_numb: '5',
+            straight: '2',
             left: '1',
             right: '1',
             turnaround: '0',
-            left_turn: '1',
+            left_turn: '0',
             straight_left: '0',
-            straight_right: '0',
-            left_straight_right: '1',
+            straight_right: '1',
+            left_straight_right: '0',
             variation: '0',
           },
         ],
         subList: [
           {
             attribute: 'signs',
-            longitude: '30.1234',
-            latitude: '114.1234',
-            east: '有',
-            south: '有',
-            west: '有',
+            longitude: '114.89745',
+            latitude: '30.56789',
+            east: '无',
+            south: '无',
+            west: '无',
             north: '有',
           },
           {
             attribute: 'guide',
-            longitude: '30.2345',
-            latitude: '114.2345',
-            east: '有',
-            south: '有',
-            west: '有',
+            longitude: '114.89745',
+            latitude: '30.56789',
+            east: '无',
+            south: '无',
+            west: '无',
             north: '有',
           },
           {
             attribute: 'isolation',
-            longitude: '30.2345',
-            latitude: '114.2345',
-            east: '有',
+            longitude: '114.89745',
+            latitude: '30.56789',
+            east: '无',
             south: '无',
-            west: '有',
-            north: '无',
+            west: '无',
+            north: '有',
           },
           {
             attribute: 'vehicleSignal',
-            longitude: '30.2345',
-            latitude: '114.2345',
-            east: '有',
-            south: '有',
-            west: '有',
+            longitude: '114.89745',
+            latitude: '30.56789',
+            east: '无',
+            south: '无',
+            west: '无',
             north: '有',
           },
           {
             attribute: 'pedeSignal',
-            longitude: '30.2345',
-            latitude: '114.2345',
-            east: '有',
-            south: '有',
-            west: '有',
+            longitude: '114.89745',
+            latitude: '30.56789',
+            east: '无',
+            south: '无',
+            west: '无',
             north: '有',
           },
           {
             attribute: 'EPMonitor',
-            longitude: '30.2345',
-            latitude: '114.2345',
-            east: '有',
-            south: '有',
-            west: '有',
+            longitude: '114.89745',
+            latitude: '30.56789',
+            east: '无',
+            south: '无',
+            west: '无',
             north: '有',
           },
           {
             attribute: 'videoMonitor',
-            longitude: '30.2345',
-            latitude: '114.2345',
-            east: '有',
-            south: '有',
-            west: '有',
+            longitude: '114.89745',
+            latitude: '30.56789',
+            east: '无 ',
+            south: '无',
+            west: '无',
             north: '有',
           },
         ],
       });
+      const subList = res.value.subList;
       const basicInfoForm = ref<FormInst | null>(null);
       const roadForm = ref<FormInst | null>(null);
 
@@ -270,7 +328,30 @@
         organizOpts,
         roadPavingOpts,
         signalBrandOpts,
+        subList,
       };
+    },
+    methods: {
+      getDeviceName(attrName) {
+        switch (attrName) {
+          case 'signs':
+            return '指路标志';
+          case 'guide':
+            return '导向信息';
+          case 'isolation':
+            return '隔离栏';
+          case 'vehicleSignal':
+            return '机动车信号灯';
+          case 'pedeSignal':
+            return '行人信号灯';
+          case 'EPMonitor':
+            return '电子警察';
+          case 'videoMonitor':
+            return '视频监控';
+          default:
+            return '未命名设备';
+        }
+      },
     },
   });
 </script>
--- src/views/crossroad/components/RoadList.vue
+++ src/views/crossroad/components/RoadList.vue
@@ -9,24 +9,86 @@
       @close="handleClose"
       @add="handleAdd"
     >
-      <n-tab-pane v-for="(road, roadIndex) in roads" :key="roadIndex" :name="roadIndex">
-        {{ road }}
+      <n-tab-pane v-for="(road, roadIndex) in roads" :key="roadIndex" :name="road.direction">
+        <n-form class="px-2" label-placement="left" :model="road">
+          <n-grid :cols="2" x-gap="8">
+            <n-form-item-gi :span="2" label="方向名" path="direction">
+              <n-input v-model:value="road.direction" type="text" placeholder="方向名" clearable />
+            </n-form-item-gi>
+
+            <n-form-item-gi label="出口车道数" path="export_numb">
+              <n-input v-model:value="road.export_numb" placeholder="出口车道数" clearable />
+            </n-form-item-gi>
+
+            <n-form-item-gi label="进口车道数" path="import_numb">
+              <n-input v-model:value="road.import_numb" placeholder="进口车道数" clearable />
+            </n-form-item-gi>
+          </n-grid>
+
+          <n-grid :cols="4" x-gap="8">
+            <n-form-item-gi label-placement="top" label="直行" path="straight">
+              <n-input v-model:value="road.straight" placeholder="直行" clearable />
+            </n-form-item-gi>
+            <n-form-item-gi label-placement="top" label="左转" path="left">
+              <n-input v-model:value="road.left" placeholder="左转" clearable />
+            </n-form-item-gi>
+            <n-form-item-gi label-placement="top" label="右转" path="right">
+              <n-input v-model:value="road.right" placeholder="右转" clearable />
+            </n-form-item-gi>
+            <n-form-item-gi label-placement="top" label="调头" path="turnaround">
+              <n-input v-model:value="road.turnaround" placeholder="调头" clearable />
+            </n-form-item-gi>
+            <n-form-item-gi label-placement="top" label="左调" path="left_turn">
+              <n-input v-model:value="road.left_turn" placeholder="左调" clearable />
+            </n-form-item-gi>
+            <n-form-item-gi label-placement="top" label="直左" path="straight_left">
+              <n-input v-model:value="road.straight_left" placeholder="直左" clearable />
+            </n-form-item-gi>
+            <n-form-item-gi label-placement="top" label="直右" path="straight_right">
+              <n-input v-model:value="road.straight_right" placeholder="直右" clearable />
+            </n-form-item-gi>
+            <n-form-item-gi label-placement="top" label="左直右" path="left_straight_right">
+              <n-input v-model:value="road.left_straight_right" placeholder="左直右" clearable />
+            </n-form-item-gi>
+            <n-form-item-gi label-placement="top" label="可变车道" path="import_numb">
+              <n-input v-model:value="road.import_numb" placeholder="可变车道" clearable />
+            </n-form-item-gi>
+          </n-grid>
+        </n-form>
       </n-tab-pane>
     </n-tabs>
   </div>
 </template>
 
 <script lang="ts">
-  import { defineComponent, toRefs, ref, computed } from 'vue';
+  import { defineComponent, toRefs, ref, computed, PropType } from 'vue';
+
+  interface Road {
+    direction: string;
+    export_numb: string;
+    import_numb: string;
+    straight: string;
+    left: string;
+    right: string;
+    turnaround: string;
+    left_turn: string;
+    straight_left: string;
+    straight_right: string;
+    left_straight_right: string;
+    variation: string;
+  }
 
   export default defineComponent({
     name: 'RoadList',
     props: {
-      roadList: Array,
+      roadList: {
+        type: Array as PropType<Road[]>,
+        required: true,
+      },
     },
     setup(props) {
       const { roadList } = toRefs(props);
-      const valueRef = ref(0);
+      const valueRef = ref(roadList.value[0].direction);
       const roadsRef = ref(roadList);
       const addableRef = computed(() => {
         return {
